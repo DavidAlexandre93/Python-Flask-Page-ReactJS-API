be3131bff5908b4dba3f3e36144d3ca949f318a0
David Alexandre Fernandes <davidalexandrefernandes@Outlook.com>
2022-03-22 00:54:06 -0300

    Page flask and react

---
+++ react-to-do/src/components/TodoForm.js
@@ -0,0 +1,41 @@
+//TodoForm.js
+import React, { useState } from 'react';
+
+function TodoForm({ addTodo }) {
+    const [userInput, setUserInput] = useState("");
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        if (!userInput) return;
+        await postData('http://localhost:5000/todo/create',
+          { complete: false, task: userInput })
+          .then(data => {
+              addTodo(data);
+              setUserInput("");
+           })
+     };
+
+    async function postData(url = '', data = {}) {
+        const response = await fetch(url, {
+           method: 'POST',
+           headers: {
+              'Content-Type': 'application/json'
+           },
+           body: JSON.stringify(data)
+        });
+        return response.json();
+     }
+
+    return (
+        <form onSubmit={handleSubmit} className="todo-form">
+            <input
+                placeholder="New Task"
+                type="text"
+                className="input"
+                value={userInput}
+                onChange={e => setUserInput(e.target.value)}
+            />
+        </form>
+    );
+};
+export default TodoForm;
---
+++ react-to-do/src/components/TodoList.js
@@ -0,0 +1,71 @@
+//TodoList
+import React from 'react';
+
+function TodoList({ todos, updateTodos }) {
+
+    const removeTask = async (index, todo) => {
+        await deleteData('http://localhost:5000/todo/delete',
+           todo)
+           .then(data => {
+              const updatedList = todos.filter((task, taskIndex) => {
+                 return taskIndex !== index;
+              });
+              updateTodos(updatedList);
+           })
+     }
+
+    const markComplete = async (index, todo) => {
+        const updated = { ...todo, complete: !todo.complete }
+        await updateData('http://localhost:5000/todo/update',
+           updated)
+           .then(data => {
+              const updatedList = todos.map((item, id) => {
+                 if (index !== id) return item;
+                 return updated;
+              });
+              updateTodos(updatedList);
+           })
+     }
+
+    async function deleteData(url = '', data = {}) {
+        const response = await fetch(url, {
+           method: 'DELETE',
+           headers: {
+              'Content-Type': 'application/json'
+           },
+           body: JSON.stringify(data)
+        });
+        return response.json();
+     }
+
+    async function updateData(url = '', data = {}) {
+        const response = await fetch(url, {
+           method: 'PUT',
+           headers: {
+              'Content-Type': 'application/json'
+           },
+           body: JSON.stringify(data)
+        });
+        return response.json();
+    }
+
+    return (
+
+        <div className="todo-list">
+            {todos.map((todo, index) => (
+                <div key={index}>
+                    <div
+                        className={`todo ${todo.complete ? "taskDone" : ""}`}
+                        onClick={() => setTodos(markComplete(index, todo))}>
+                        Item {index + 1}: {todo.task}
+                    </div>
+                    <button className="button" onClick={() => removeTask(index, todo)}>Delete</button>
+
+                </div>
+
+            ))}
+        </div>
+    );
+};
+
+export default TodoList;
